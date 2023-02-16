38e5108bdf36afb09bc3dd8e8eee652aa179c812
h1romas4 <h1romas4@gmail.com>
2023-02-16 19:42:56 +0900

    hello world

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,17 @@
 #include <Arduino.h>
-#include <M5GFX.h>
+#include <M5Core2.h>
 
 static const char *TAG = "main.cpp";
 
-/**
- * M5GFX member
- */
-M5GFX display;
-
 void setup(void)
 {
-    display.begin();
+    M5.begin();
 }
 
 void loop(void)
 {
+    M5.update();
+
     ESP_LOGI(TAG, "Hello, M5Stack Core2 world.");
 
     delay(500);
