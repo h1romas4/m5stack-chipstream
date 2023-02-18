520c7f7ea6b130fec303b56d8644c58538ee67b4
h1romas4 <h1romas4@gmail.com>
2023-02-18 17:13:44 +0900

    split function

--- main/main.cpp
+++ main/main.cpp
@@ -66,6 +66,100 @@ void init_module_rca_i2s(void)
 }
 
 /**
+ * initalize vgm
+ */
+esp_err_t init_vgm(const char* filename, uint32_t instance_id, uint32_t mem_id)
+{
+    // SD open
+    File fp = SD.open(filename);
+    size_t vgm_size = fp.size();
+    ESP_LOGI(TAG, "vgm file(%d)", vgm_size);
+
+    // alloc vgm data
+    memory_alloc(mem_id, vgm_size);
+    uint8_t *vgm_alloc_address  = memory_get_ref(mem_id);
+    ESP_LOGI(TAG, "memory_get_ref(%p)", vgm_alloc_address);
+
+    // load vgm
+    size_t read_vgm_size = fp.read(vgm_alloc_address, vgm_size);
+    ESP_LOGI(TAG, "read vgm file(%d)", read_vgm_size);
+    fp.close();
+    if(vgm_size != read_vgm_size) {
+        ESP_LOGE(TAG, "read vgm error(%d)", read_vgm_size);
+    }
+
+    // heap wwatch
+    heap_caps_print_heap_info(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL | MALLOC_CAP_DEFAULT);
+
+    // create vgm instance
+    bool vgm_result = vgm_create(
+        instance_id,
+        CHIPSTREAM_SAPMLING_RATE,
+        CHIPSTREAM_SAMPLE_CHUNK_SIZE,
+        mem_id);
+    ESP_LOGI(TAG, "vgm_create(%d)", vgm_result);
+
+    // drop vgm data (clone by vgm_create)
+    memory_drop(mem_id);
+
+    // get json
+    if(vgm_result) {
+        uint32_t memory_index_id = vgm_get_gd3_json(instance_id);
+        uint8_t *vgm_header_json  = memory_get_ref(memory_index_id);
+        uint32_t vgm_heade_json_len = memory_get_len(memory_index_id);
+        uint8_t *vgm_header = (uint8_t *)calloc(vgm_heade_json_len + 1, sizeof(uint8_t));
+        strncpy((char *)vgm_header, (const char *)vgm_header_json, vgm_heade_json_len);
+        ESP_LOGI(TAG, "vgm_create(%s)", vgm_header);
+        free(vgm_header);
+        memory_drop(memory_index_id);
+    }
+
+    if(!vgm_result) {
+        return ESP_FAIL;
+    }
+
+    return ESP_OK;
+}
+
+void stream_vgm(uint32_t instance_id) {
+    /**
+     * M5Stack Core2 (ESP32) Test Result
+     *
+     * ymfm YM2151(on Flash) and X68K OKI
+     *  I (4365) main.cpp: render time: 44100 / 2001ms (*Takes x2 as long as real time)
+     *  I (6540) main.cpp: render time: 44100 / 2105ms
+     * ymfm YM2151(on IRAM) and X68K OKI
+     *  I (4356) main.cpp: render time: 44100 / 1988ms (*Takes x2 as long as real time)
+     *  I (6521) main.cpp: render time: 44100 / 2094ms
+     * only X68K OKI
+     *  I (2873) main.cpp: render time: 44100 / 522ms  (*This speed is well in time)
+     *  I (3443) main.cpp: render time: 44100 / 501ms
+     *
+     * The time is about 400ms shorter for the ESP32-S3 with PSRAM set to 80 MHz Octa.
+     */
+    uint32_t time = millis();
+
+    uint32_t loop_count = vgm_play(instance_id);
+    int16_t *s16le = vgm_get_sampling_s16le_ref(instance_id);
+    // output i2s (test)
+    size_t written = 0;
+    ESP_ERROR_CHECK(i2s_write(I2S_NUM_1, s16le, SAMPLE_BUF_LEN, &written, 0));
+
+    ESP_LOGI(TAG, "written %d / %d (%04x): render time: %d / %dms",
+        written,
+        SAMPLE_BUF_LEN,
+        (uint16_t)s16le[0],
+        CHIPSTREAM_SAMPLE_CHUNK_SIZE,
+        (uint32_t)(millis() - time));
+}
+
+void drop_vgm(uint32_t instance_id)
+{
+    // drop vgm instance
+    vgm_drop(CHIPSTREAM_VGM_INDEX_ID);
+}
+
+/**
  * Arduino setup
  */
 void setup(void)
@@ -82,91 +176,17 @@ void setup(void)
     // workaround disable watchdog
     esp_task_wdt_init(3600, false);
 
-    // SD open
-    File fp = SD.open("/M5Stack/VGM/05.vgm");
-    size_t vgm_size = fp.size();
-    ESP_LOGI(TAG, "vgm file(%d)", vgm_size);
-
-    // alloc vgm data
-    memory_alloc(CHIPSTREAM_MEMORY_INDEX_ID, vgm_size);
-    uint8_t *vgm_alloc_address  = memory_get_ref(CHIPSTREAM_MEMORY_INDEX_ID);
-    ESP_LOGI(TAG, "memory_get_ref(%p)", vgm_alloc_address);
-
-    // load vgm
-    size_t read_vgm_size = fp.read(vgm_alloc_address, vgm_size);
-    ESP_LOGI(TAG, "read vgm file(%d)", read_vgm_size);
-    fp.close();
-    if(vgm_size != read_vgm_size) {
-        ESP_LOGE(TAG, "read vgm error(%d)", read_vgm_size);
-    }
-
-    // heap wwatch
-    heap_caps_print_heap_info(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL | MALLOC_CAP_DEFAULT);
-
-    // create vgm instance
-    bool vgm_result = vgm_create(
+    // init vgm
+    ESP_ERROR_CHECK(
+        init_vgm("/M5Stack/VGM/05.vgm",
         CHIPSTREAM_VGM_INDEX_ID,
-        CHIPSTREAM_SAPMLING_RATE,
-        CHIPSTREAM_SAMPLE_CHUNK_SIZE,
-        CHIPSTREAM_MEMORY_INDEX_ID);
-    ESP_LOGI(TAG, "vgm_create(%d)", vgm_result);
+        CHIPSTREAM_MEMORY_INDEX_ID));
 
-    // drop vgm data (clone by vgm_create)
-    memory_drop(CHIPSTREAM_MEMORY_INDEX_ID);
+    // stream (test 1 tick)
+    stream_vgm(CHIPSTREAM_VGM_INDEX_ID);
 
-    // get json
-    if(vgm_result) {
-        uint32_t memory_index_id = vgm_get_gd3_json(CHIPSTREAM_VGM_INDEX_ID);
-        uint8_t *vgm_header_json  = memory_get_ref(memory_index_id);
-        uint32_t vgm_heade_json_len = memory_get_len(memory_index_id);
-        uint8_t *vgm_header = (uint8_t *)calloc(vgm_heade_json_len + 1, sizeof(uint8_t));
-        strncpy((char *)vgm_header, (const char *)vgm_header_json, vgm_heade_json_len);
-        ESP_LOGI(TAG, "vgm_create(%s)", vgm_header);
-        free(vgm_header);
-        memory_drop(memory_index_id);
-    }
-
-    // play
-    if(vgm_result) {
-        // loop one in vgm
-        uint32_t loop_count = 0;
-        while(loop_count == 0) {
-            /**
-             * M5Stack Core2 (ESP32) Test Result
-             *
-             * ymfm YM2151(on Flash) and X68K OKI
-             *  I (4365) main.cpp: render time: 44100 / 2001ms (*Takes x2 as long as real time)
-             *  I (6540) main.cpp: render time: 44100 / 2105ms
-             * ymfm YM2151(on IRAM) and X68K OKI
-             *  I (4356) main.cpp: render time: 44100 / 1988ms (*Takes x2 as long as real time)
-             *  I (6521) main.cpp: render time: 44100 / 2094ms
-             * only X68K OKI
-             *  I (2873) main.cpp: render time: 44100 / 522ms  (*This speed is well in time)
-             *  I (3443) main.cpp: render time: 44100 / 501ms
-             *
-             * The time is about 400ms shorter for the ESP32-S3 with PSRAM set to 80 MHz Octa.
-             */
-            uint32_t time = millis();
-
-            loop_count = vgm_play(CHIPSTREAM_VGM_INDEX_ID);
-            int16_t *s16le = vgm_get_sampling_s16le_ref(CHIPSTREAM_VGM_INDEX_ID);
-            // output i2s (test)
-            size_t written = 0;
-            ESP_ERROR_CHECK(i2s_write(I2S_NUM_1, s16le, SAMPLE_BUF_LEN, &written, 0));
-
-            // ESP_LOGI(TAG, "written %d / %d (%04x): render time: %d / %dms",
-            //     written,
-            //     SAMPLE_BUF_LEN,
-            //     (uint16_t)s16le[0],
-            //     CHIPSTREAM_SAMPLE_CHUNK_SIZE,
-            //     (uint32_t)(millis() - time));
-        }
-    }
-
-    // drop vgm instance
-    if(vgm_result) {
-        vgm_drop(CHIPSTREAM_VGM_INDEX_ID);
-    }
+    // drop
+    drop_vgm(CHIPSTREAM_VGM_INDEX_ID);
 }
 
 /**
