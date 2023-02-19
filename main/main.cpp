61800b7c636f74ad6c4847b97f076c8074d82862
h1romas4 <h1romas4@gmail.com>
2023-02-19 19:14:34 +0900

    sep module

--- main/main.cpp
+++ main/main.cpp
@@ -6,36 +6,23 @@
 #include <Arduino.h>
 #include <M5Core2.h>
 #include <esp_task_wdt.h>
-#include <driver/i2s.h>
+
+#include "module_rca_i2s.h"
+#include "chipstream.h"
 
 static const char *TAG = "main.cpp";
 
-/**
- * chipstream(vgmplay) interface test
- *
- * Originally, bindgen is used to generate them.
- */
-extern "C" void memory_alloc(uint32_t memory_index_id, uint32_t length);
-extern "C" uint8_t* memory_get_ref(uint32_t memory_index_id);
-extern "C" uint32_t memory_get_len(uint32_t memory_index_id);
-extern "C" void memory_drop(uint32_t memory_index_id);
-extern "C" bool vgm_create(uint32_t vgm_index_id, uint32_t output_sampling_rate, uint32_t output_sample_chunk_size, uint32_t memory_index_id);
-extern "C" uint32_t vgm_get_gd3_json(uint32_t vgm_index_id);
-extern "C" int16_t* vgm_get_sampling_s16le_ref(uint32_t vgm_index_id);
-extern "C" uint32_t vgm_play(uint32_t vgm_index_id);
-extern "C" void vgm_drop(uint32_t vgm_index_id);
-
-#define CHIPSTREAM_MEMORY_INDEX_ID 0
-#define CHIPSTREAM_VGM_INDEX_ID 0
-#define CHIPSTREAM_SAPMLING_RATE 44100
-#define CHIPSTREAM_SAMPLE_CHUNK_SIZE 128
-
 #define STREO 2
-#define SAMPLE_BUF_LEN (CHIPSTREAM_SAMPLE_CHUNK_SIZE * STREO * sizeof(int16_t))
-#define RING_BUF_LEN 5
+#define SAPMLING_RATE 44100
+#define SAMPLE_CHUNK_SIZE 128
+#define SAMPLE_BUF_LEN (SAMPLE_CHUNK_SIZE * STREO * sizeof(int16_t))
+
+// for test
+#define CS_MEM_INDEX_ID 0
+#define CS_VGM_INSTANCE_ID 0
 
 /**
- * vgmplay (unsafe)
+ * Play state
  */
 typedef enum {
   STRAT,
@@ -45,99 +32,37 @@ typedef enum {
 } vgm_state_t;
 
 vgm_state_t vgm_state;
-uint32_t ring_buffer[RING_BUF_LEN][CHIPSTREAM_SAMPLE_CHUNK_SIZE];
-uint32_t ring_index = 0;
-int32_t ring_play_index = 0;
 
-/**
- * Module RCA I2S initilize
- */
-void init_module_rca_i2s(void)
-{
-    // i2s_driver_install
-    i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-        .sample_rate = CHIPSTREAM_SAPMLING_RATE,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 2,
-        .dma_buf_len = SAMPLE_BUF_LEN,
-        .use_apll = false,
-        .tx_desc_auto_clear = true,
-        .fixed_mclk = I2S_PIN_NO_CHANGE
-    };
-    ESP_ERROR_CHECK(i2s_driver_install(I2S_NUM_1, &i2s_config, 0, NULL));
-
-    // i2s_set_pin
-    i2s_pin_config_t i2s_pin_config = {
-        .mck_io_num = GPIO_NUM_0,
-        .bck_io_num = GPIO_NUM_19,
-        .ws_io_num = GPIO_NUM_0,
-        .data_out_num = GPIO_NUM_2,
-        .data_in_num = I2S_PIN_NO_CHANGE
-    };
-    ESP_ERROR_CHECK(i2s_set_pin(I2S_NUM_1, &i2s_pin_config));
-}
-
-/**
- * initalize vgm
- */
-esp_err_t init_vgm(const char* filename, uint32_t instance_id, uint32_t mem_id)
+void load_sd_vgm_file(const char *filename, uint32_t vgm_instance_id, uint32_t vgm_mem_id)
 {
     // SD open
     File fp = SD.open(filename);
     size_t vgm_size = fp.size();
     ESP_LOGI(TAG, "vgm file(%d)", vgm_size);
 
-    // alloc vgm data
-    memory_alloc(mem_id, vgm_size);
-    uint8_t *vgm_alloc_address  = memory_get_ref(mem_id);
-    ESP_LOGI(TAG, "memory_get_ref(%p)", vgm_alloc_address);
+    // alloc vgmfile mem
+    uint8_t* mem = cs_alloc_mem(CS_MEM_INDEX_ID, vgm_size);
 
-    // load vgm
-    size_t read_vgm_size = fp.read(vgm_alloc_address, vgm_size);
+    // load vgm from SD
+    size_t read_vgm_size = fp.read(mem, vgm_size);
     ESP_LOGI(TAG, "read vgm file(%d)", read_vgm_size);
     fp.close();
     if(vgm_size != read_vgm_size) {
         ESP_LOGE(TAG, "read vgm error(%d)", read_vgm_size);
     }
 
-    // heap wwatch
-    heap_caps_print_heap_info(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL | MALLOC_CAP_DEFAULT);
-
     // create vgm instance
-    bool vgm_result = vgm_create(
-        instance_id,
-        CHIPSTREAM_SAPMLING_RATE,
-        CHIPSTREAM_SAMPLE_CHUNK_SIZE,
-        mem_id);
-    ESP_LOGI(TAG, "vgm_create(%d)", vgm_result);
+    cs_create_vgm(
+        vgm_mem_id,
+        vgm_instance_id,
+        SAPMLING_RATE,
+        SAMPLE_CHUNK_SIZE);
 
-    // drop vgm data (clone by vgm_create)
-    memory_drop(mem_id);
-
-    // get json
-    if(vgm_result) {
-        uint32_t memory_index_id = vgm_get_gd3_json(instance_id);
-        uint8_t *vgm_header_json  = memory_get_ref(memory_index_id);
-        uint32_t vgm_heade_json_len = memory_get_len(memory_index_id);
-        uint8_t *vgm_header = (uint8_t *)calloc(vgm_heade_json_len + 1, sizeof(uint8_t));
-        strncpy((char *)vgm_header, (const char *)vgm_header_json, vgm_heade_json_len);
-        ESP_LOGI(TAG, "vgm_create(%s)", vgm_header);
-        free(vgm_header);
-        memory_drop(memory_index_id);
-    }
-
-    if(!vgm_result) {
-        return ESP_FAIL;
-    }
-
-    return ESP_OK;
+    // vgm data is cloned and decoded by vgm instance
+    cs_drop_mem(CS_MEM_INDEX_ID);
 }
 
-uint32_t stream_vgm(uint32_t instance_id) {
+uint32_t stream_vgm(uint32_t vgm_instance_id) {
     /**
      * M5Stack Core2 (ESP32) Test Result
      *
@@ -155,28 +80,16 @@ uint32_t stream_vgm(uint32_t instance_id) {
      */
     uint32_t time = millis();
 
-    uint32_t loop_count = vgm_play(instance_id);
-    int16_t *s16le = vgm_get_sampling_s16le_ref(instance_id);
+    uint32_t loop_count;
+    int16_t *s16le = cs_stream_vgm(vgm_instance_id, &loop_count);
 
     ESP_LOGI(TAG, "written %d (%04x): render time: %d / %dms",
         SAMPLE_BUF_LEN,
         (uint16_t)s16le[0],
-        CHIPSTREAM_SAMPLE_CHUNK_SIZE,
+        SAMPLE_CHUNK_SIZE,
         (uint32_t)(millis() - time));
 
     return loop_count;
-}
-
-bool buffer_vgm(uint32_t instance_id)
-{
-    if(stream_vgm(instance_id) > 0) return true;
-    return false;
-}
-
-void drop_vgm(uint32_t instance_id)
-{
-    // drop vgm instance
-    vgm_drop(instance_id);
 }
 
 /**
@@ -191,7 +104,16 @@ void setup(void)
     i2s_driver_uninstall(I2S_NUM_0);
 
     // initialize Module RCA I2S
-    init_module_rca_i2s();
+    init_module_rca_i2s(SAPMLING_RATE, SAMPLE_CHUNK_SIZE, 2);
+
+    // load_sd_vgm_file
+    load_sd_vgm_file("/M5STACK/VGM/05.vgm", CS_VGM_INSTANCE_ID, CS_MEM_INDEX_ID);
+
+    // heap wwatch
+    heap_caps_print_heap_info(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL | MALLOC_CAP_DEFAULT);
+
+    // test
+    stream_vgm(CS_VGM_INSTANCE_ID);
 
     // set state
     vgm_state = vgm_state_t::STRAT;
@@ -206,23 +128,21 @@ void loop(void)
 
     switch (vgm_state) {
         case vgm_state_t::STRAT:
-            // init vgm
-            ESP_ERROR_CHECK(
-                init_vgm("/M5Stack/VGM/05.vgm",
-                CHIPSTREAM_VGM_INDEX_ID,
-                CHIPSTREAM_MEMORY_INDEX_ID));
-            // pre buffer
-            buffer_vgm(CHIPSTREAM_VGM_INDEX_ID);
+            // load and init vgm instance
+            load_sd_vgm_file(
+                "/M5Stack/VGM/05.vgm",
+                CS_VGM_INSTANCE_ID,
+                CS_MEM_INDEX_ID);
             vgm_state = vgm_state_t::PLAYING;
             break;
         case vgm_state_t::PLAYING:
-            // buffring
-            if(buffer_vgm(CHIPSTREAM_VGM_INDEX_ID)) {
+            // stream
+            if(stream_vgm(CS_VGM_INSTANCE_ID) > 0) {
                 vgm_state = vgm_state_t::END;
             }
             break;
         case vgm_state_t::END:
-            drop_vgm(CHIPSTREAM_VGM_INDEX_ID);
+            cs_drop_vgm(CS_VGM_INSTANCE_ID);
             vgm_state = vgm_state_t::SLEEP;
             break;
         default:
@@ -230,8 +150,5 @@ void loop(void)
             delay(999);
             break;
     }
-    // output i2s (test)
-    // size_t written = 0;
-    // ESP_ERROR_CHECK(i2s_write(I2S_NUM_1, s16le, SAMPLE_BUF_LEN, &written, 0));
     delay(1);
 }
---
+++ main/module_rca_i2s.c
@@ -0,0 +1,42 @@
+#include <esp_err.h>
+#include <driver/i2s.h>
+
+static const char *TAG = "module_rca_i2s.c";
+
+/**
+ * Module RCA I2S initilize
+ */
+void init_module_rca_i2s(uint32_t sample_rate, uint32_t dma_buf_len, uint32_t dma_buf_count)
+{
+    // i2s_driver_install
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = sample_rate,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = dma_buf_count,
+        .dma_buf_len = dma_buf_len,
+        .use_apll = false,
+        .tx_desc_auto_clear = true,
+        .fixed_mclk = I2S_PIN_NO_CHANGE
+    };
+    ESP_ERROR_CHECK(i2s_driver_install(I2S_NUM_1, &i2s_config, 0, NULL));
+
+    // i2s_set_pin
+    i2s_pin_config_t i2s_pin_config = {
+        .mck_io_num = GPIO_NUM_0,
+        .bck_io_num = GPIO_NUM_19,
+        .ws_io_num = GPIO_NUM_0,
+        .data_out_num = GPIO_NUM_2,
+        .data_in_num = I2S_PIN_NO_CHANGE
+    };
+    ESP_ERROR_CHECK(i2s_set_pin(I2S_NUM_1, &i2s_pin_config));
+}
+
+void write_i2s(int16_t *s16le, uint32_t len)
+{
+    size_t written = 0;
+    ESP_ERROR_CHECK(i2s_write(I2S_NUM_1, s16le, len, &written, 0));
+}
