bcd987c69005687be2e2abe64068388777b4f6d9
h1romas4 <h1romas4@gmail.com>
2023-02-19 19:59:22 +0900

    lipstick

--- main/chipstream.h
+++ main/chipstream.h
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
 extern "C" {
-uint8_t* cs_alloc_mem(uint32_t mem_id, uint32_t vgm_size);
-void cs_drop_mem(uint32_t vgm_mem_id);
 bool cs_create_vgm(uint32_t vgm_mem_id, uint32_t vgm_instance_id, uint32_t sample_rate, uint32_t sample_chunk_size);
 int16_t* cs_stream_vgm(uint32_t vgm_instance_id, uint32_t *loop_count);
 void cs_drop_vgm(uint32_t vgm_instance_id);
+uint8_t* cs_alloc_mem(uint32_t mem_id, uint32_t vgm_size);
+void cs_drop_mem(uint32_t vgm_mem_id);
 }
